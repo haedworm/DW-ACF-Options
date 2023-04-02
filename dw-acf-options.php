59aa63b5105534110fa3076ac933fbd24cc36813
haedworm <haedworm@gmail.com>
2023-04-02 17:09:56 +0100

    Update dw-acf-options.php

--- dw-acf-options.php
+++ dw-acf-options.php
@@ -8,4 +8,15 @@ Author: dazzie
 Author URI: https://dwoodley.co.uk/
 */
 
-
+// Add ACF Options Page as a sub-menu of Settings
+if( function_exists('acf_add_options_page') ) {
+    acf_add_options_page(array(
+        'page_title' => 'Global ACF Fields',
+        'menu_title' => 'Global ACF Fields',
+        'menu_slug' => 'dw-acf-options',
+        'capability' => 'manage_options',
+        'position' => 30,
+        'icon_url' => 'dashicons-admin-generic',
+        'parent_slug' => 'options-general.php',
+    ));
+}
